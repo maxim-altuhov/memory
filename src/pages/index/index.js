f781201cdc2312f276cafa2eefb26648c1d9706d
Maxim A <m_altuhov@mail.ru>
2021-07-13 14:52:55 +0300

    Прописал основную логику работы

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,25 +5,50 @@ window.jQuery = $;
 window.$ = $;
 
 function initFunction() {
+  let numberOfCards;
 
-  function initControl(selector, numberOfCards) {
-    const controls = $(selector);
+  // функ. для перемешивания случайным образом эл-тов массива
+  function shuffle(array) {
+    let currentIndex = array.length;
+    let temporaryValue;
+    let randomIndex;
 
-    controls.on('click', function() {
-      if (!$(this).hasClass('control__btn_active')) {
-        controls.removeClass('control__btn_active');
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-        $(this).addClass('control__btn_active');
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
 
-        if (selector === '.simplicity button') {
-          numberOfCards = $(this).text();
-          new Cards().remove();
-          new Cards(numberOfCards).render();
-        }
-      }
-    });
+    return array;
+  }
 
-    if (numberOfCards) new Cards(numberOfCards).render();
+  // переключение и сравнение карточек
+  function checkingCards() {
+    function checkThisCard(classDisabled = '') {
+      const $cards = $('.card');
+
+      $cards.addClass('not-click');
+      setTimeout(() => {
+        $('.is-flipped').removeClass('is-flipped').addClass(classDisabled);
+        $cards.removeClass('not-click');
+      }, 800);
+    }
+
+    let flippedCard;
+
+    if ($('.is-flipped').length < 2) {
+      $(this).toggleClass('is-flipped');
+      flippedCard = $('.is-flipped');
+    }
+
+    if (flippedCard.length === 2 && flippedCard.eq(0).text() !== flippedCard.eq(1).text()) {
+      checkThisCard();
+    } else if (flippedCard.length === 2 && flippedCard.eq(0).text() === flippedCard.eq(1).text()) {
+      checkThisCard('disabled');
+    }
   }
 
   // Класс для работы с карточками
@@ -32,31 +57,69 @@ function initFunction() {
       this.quantity = quantity;
     }
 
+    // формирование карточек
     render() {
       const fragment = document.createDocumentFragment();
+      const inputArray = Array.from({ length: 100 }, () => Math.floor(Math.random() * 100));
+      let arrayResult = [];
+
+      for (; arrayResult.length < numberOfCards;) {
+        const randomValue = inputArray[Math.floor(Math.random() * inputArray.length)];
+
+        if (!arrayResult.includes(randomValue)) arrayResult.push(randomValue, randomValue);
+      }
+
+      arrayResult = shuffle(arrayResult);
 
       for (let index = 0; index < this.quantity; index++) {
         const element = document.createElement('div');
         $(element).addClass('card');
-        $(element).html(`<div class="card__face card__face_front"></div><div class="card__face card__face_back">Text</div>`);
-
-        $(element).on('click', () => {
-          $(element).toggleClass('is-flipped');
-        });
+        $(element).html(`<div class="card__face card__face_front"></div><div class="card__face card__face_back">${arrayResult[index]}</div>`);
+        $(element).on('click', checkingCards);
 
         fragment.append(element);
       }
 
-      $('.memory__wrapper').append(fragment);
+      $('.loading').fadeIn().fadeOut();
+      $('.memory__wrapper')
+        .css('display', 'none')
+        .append(fragment)
+        .delay(800)
+        .fadeIn(800);
     }
 
-    remove() {
+    // удаление всех карточек
+    static remove() {
       $('.card').remove();
     }
   }
 
+  // инициализация панели с контроллерами
+  function initControl(selector, startNumberOfCards) {
+    const $controls = $(selector);
+    numberOfCards = startNumberOfCards;
+
+    function keepTrackOfControls() {
+      if (!$(this).hasClass('control__btn_active')) {
+        $controls.removeClass('control__btn_active');
+
+        $(this).addClass('control__btn_active');
+
+        if (selector === '.simplicity button') {
+          numberOfCards = $(this).text();
+          Cards.remove();
+          new Cards(numberOfCards).render();
+        }
+      }
+    }
+
+    $controls.on('click', keepTrackOfControls);
+
+    if (startNumberOfCards) new Cards(startNumberOfCards).render();
+  }
+
   // вызов функций и методов
-  initControl('.simplicity button', 8);
+  initControl('.simplicity button', 12);
   initControl('.type button');
 }
 
